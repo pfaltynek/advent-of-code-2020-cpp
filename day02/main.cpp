95bf28c9510ffb90627461f5839d020a38921664
Petr Faltýnek <pfaltynek@gmail.com>
2020-12-02 18:05:30 +0100

    day02 - part1 finished

--- day02/main.cpp
+++ day02/main.cpp
@@ -5,8 +5,8 @@
 const std::regex PASSWORD_RULE("^(\\d+)-(\\d+) ([a-z]): ([a-z]+)$");
 
 struct password_rule {
-	int32_t min;
-	int32_t max;
+	uint32_t min;
+	uint32_t max;
 	char chr;
 	std::string password;
 };
@@ -21,6 +21,8 @@ class AoC2020_day02 : public AoC {
 	int32_t get_aoc_year();
 
   private:
+	bool is_rule_valid(const password_rule& rule);
+	uint32_t get_valid_password_rules_count();
 	std::vector<password_rule> rules_;
 };
 
@@ -47,21 +49,56 @@ bool AoC2020_day02::init(const std::vector<std::string> lines) {
 	return true;
 }
 
+bool AoC2020_day02::is_rule_valid(const password_rule& rule) {
+	uint32_t count = 0;
+	size_t last_pos = 0;
+
+	do {
+		last_pos = rule.password.find(rule.chr, last_pos);
+		if (last_pos != std::string::npos) {
+			last_pos++;
+			count++;
+		} else {
+			break;
+		}
+	} while (true);
+
+	if ((count <= rule.max) && (count >= rule.min)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+uint32_t AoC2020_day02::get_valid_password_rules_count() {
+	uint32_t result = 0;
+	for (size_t i = 0; i < rules_.size(); i++)
+	{
+		if (is_rule_valid(rules_[i])) {
+			result++;
+		}
+	}
+
+	return result;
+}
+
 void AoC2020_day02::tests() {
-	int64_t result;
+	uint32_t result;
 
 	if (init({"1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"})) {
+		result = get_valid_password_rules_count(); // 2
 	}
 }
 
 bool AoC2020_day02::part1() {
-	int64_t result = 0;
 
-	return false;
+	result1_ = std::to_string(get_valid_password_rules_count());
+
+	return true;
 }
 
 bool AoC2020_day02::part2() {
-	int64_t result = 0;
+	uint32_t result = 0;
 
 	return false;
 }
