5f63663d27fb6a3caa713d2d81eb32e8074a6279
Petr Faltýnek <pfaltynek@gmail.com>
2021-01-25 12:42:45 +0100

    day19 - part1 finished

--- day19/main.cpp
+++ day19/main.cpp
@@ -20,6 +20,7 @@ class AoC2020_day19 : public AoC {
   private:
 	void generate_all_valid_messaages();
 	std::vector<std::string> process_rule(const uint32_t rule_no);
+	uint32_t get_count_of_matching_messages();
 	std::vector<std::string> messages_, all_valid_messages_;
 	std::map<uint32_t, std::vector<std::vector<uint32_t>>> rules_;
 	std::map<uint32_t, char> letters_;
@@ -77,22 +78,51 @@ bool AoC2020_day19::init(const std::vector<std::string> lines) {
 
 std::vector<std::string> AoC2020_day19::process_rule(const uint32_t rule_no) {
 	std::string letter = "";
-	std::vector<std::string> subresult;
+	std::vector<std::string> subresult, result = {};
 
 	if (letters_.count(rule_no)) {
 		letter += letters_[rule_no];
 		return {letter};
 	} else {
-		std::vector<std::string> one_rule;
+		std::vector<std::string> one_rule, tmp;
 
 		for (size_t i = 0; i < rules_[rule_no].size(); i++) {
 			one_rule = {};
 			for (size_t j = 0; j < rules_[rule_no][i].size(); j++) {
 				subresult = process_rule(rules_[rule_no][i][j]);
-				
+
+				if (one_rule.size()) {
+					tmp.clear();
+
+					for (size_t x = 0; x < one_rule.size(); x++) {
+						for (size_t y = 0; y < subresult.size(); y++) {
+							tmp.push_back(one_rule[x] + subresult[y]);
+						}
+					}
+
+					one_rule = tmp;
+				} else {
+					one_rule = subresult;
+				}
 			}
+
+			result.insert(result.end(), one_rule.begin(), one_rule.end());
+		}
+
+		return result;
+	}
+}
+
+uint32_t AoC2020_day19::get_count_of_matching_messages() {
+	uint32_t result = 0;
+
+	for (size_t i = 0; i < messages_.size(); i++) {
+		if (std::find(all_valid_messages_.begin(), all_valid_messages_.end(), messages_[i]) != all_valid_messages_.end()) {
+			result++;
 		}
 	}
+
+	return result;
 }
 
 void AoC2020_day19::generate_all_valid_messaages() {
@@ -107,15 +137,20 @@ void AoC2020_day19::tests() {
 
 	if (init({"0: 4 1 5", "1: 2 3 | 3 2", "2: 4 4 | 5 5", "3: 4 5 | 5 4", "4: \"a\"", "5: \"b\"", "", "aaaabb", "aaabab", "abbabb", "abbbab", "aabaab",
 			  "aabbbb", "abaaab", "ababbb"})) {
+		generate_all_valid_messaages();
+		result = get_count_of_matching_messages(); // 8
 	}
 
 	if (init({"0: 4 1 5", "1: 2 3 | 3 2", "2: 4 4 | 5 5", "3: 4 5 | 5 4", "4: \"a\"", "5: \"b\"", "", "ababbb", "bababa", "abbbab", "aaabbb", "aaaabbb"})) {
+		generate_all_valid_messaages();
+		result = get_count_of_matching_messages(); // 2
 	}
 }
 
 bool AoC2020_day19::part1() {
 
-	result1_ = std::to_string(0);
+	generate_all_valid_messaages();
+	result1_ = std::to_string(get_count_of_matching_messages());
 
 	return true;
 }
